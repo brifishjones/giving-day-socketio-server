275d21eb119eb303c435f4266852785991951e97
Brian Jones <jonebr01@gmail.com>
2018-11-17 14:34:51 -0600

    donors by state json data added with simulated changes

--- files/server.js
+++ files/server.js
@@ -10,14 +10,14 @@ const options = {
     // for this demo it is the same as cert:
     ca: fs.readFileSync('/etc/apache2/ssl/server.crt'),
     requestCert: true,
-    rejectUnauthorized: false
+    rejectUnauthorized: false 
 };
 var connections = [];
-var app = https.createServer(options);
+var app = https.createServer(options); 
 var io = require('socket.io')(app);
 var prev_total = 15000;
 var total = 15000;
-var donors = 100;
+var donors = 100; 
 app.listen(port);
 console.log('%s: server started ...', Date());
 var timer = setInterval(function() {
@@ -26,27 +26,37 @@ var timer = setInterval(function() {
       total = parseInt(content);
       if (total > 0 /* && total != prev_total */) {  // comment for demo only
         console.log("%s: total amount = $%s", Date(), total.toString());
-        io.sockets.emit('update-totals', {total: total.toString()});
-        fs.readFile('/var/tmp/state-totals.txt', 'ascii', function(err, content) {
+        io.sockets.emit('update-totals', {total: total.toString()});  
+        fs.readFile('/var/tmp/giving-day-challenge-totals.txt', 'ascii', function(err, content) {
           var myjson = JSON.stringify(content);
-          console.log(myjson);
           var obj = JSON.parse(content);
-          console.log("HERE")
-          // donors = obj['young-alumni']['donors'];  // get the number of donors for demo only
-          io.sockets.emit('update-state-totals', obj);
+          donors = obj['young-alumni']['donors'];  // get the number of donors for demo only
+          io.sockets.emit('update-challenges', obj);  
+        });
+        fs.readFile('/var/tmp/giving-day-donors-by-state.txt', 'ascii', function(err, content) {
+          var myjson = JSON.stringify(content);
+          var obj = JSON.parse(content);
+          io.sockets.emit('update-donors-by-state', obj);  
         });
       }
       prev_total = total;
     });
-    // for this demo update totals
-    // total = prev_total + Math.floor(Math.random() * 100) + 1;
-    // fs.writeFile('/var/tmp/giving-day-total.txt', total.toString() , function (err) {
-    //   if (err) throw err;
-    // });
-    // // for this demo update young-alumni challenge totals
-    // fs.writeFile('/var/tmp/giving-day-challenge-totals.txt', '{"young-alumni":  {"total":0, "donors":' + ((donors + 1) % 1000) + ', "challenge_amount":0, "challenge_donors":1000}}' , function (err) {
-    //   if (err) throw err;
-    // });
+    // for this demo update totals 
+    total = prev_total + Math.floor(Math.random() * 100) + 1;
+    fs.writeFile('/var/tmp/giving-day-total.txt', total.toString() , function (err) {
+      if (err) throw err;
+    }); 
+    // for this demo update young-alumni challenge totals 
+    fs.writeFile('/var/tmp/giving-day-challenge-totals.txt', '{"young-alumni":  {"total":0, "donors":' + ((donors + 1) % 1000) + ', "challenge_amount":0, "challenge_donors":1000}}' , function (err) {
+      if (err) throw err;
+    });
+    // for this demo update donors from each state totals 
+    var states = [ 'AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY' ]; 
+    // assign a random donor number for each state to a state key
+    var donorsbystate = states.reduce(function(st, val) { st[val] =  Math.floor(Math.random() * 3); ; return st; }, {}); 
+    fs.writeFile('/var/tmp/giving-day-donors-by-state.txt', JSON.stringify(donorsbystate) , function (err) {
+      if (err) throw err;
+    }); 
   }
 }, 15000);
 io.on('connection', function (socket) {
